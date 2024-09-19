c55ea8726c03d16ac378cf73f49114f1c6995c06
aris1377 <bek77519101@gmail.com>
2024-09-19 14:06:32 +0900

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,21 @@ import { NextPage } from "next";
 
 const  Home: NextPage = () => {
   return (
-      <Stack>
-      <Stack flexDirection={"column"}>
+      
+    <Stack className={"Home-page"}>
+      
         <Stack>
           <Stack className="container">Popular Properties</Stack>
-        </Stack>
+      </Stack>
+      
         <Stack>
           <Stack className="container">Top Agent</Stack>
-        </Stack>
+      </Stack>
+      
         <Stack>
           <Stack className="container">Top Properties</Stack>
-        </Stack>
-        </Stack>
+      </Stack>
+      
       </Stack>
   );
 }
