276435c3cc809758c2efe62447c3c25eb0989c97
aris1377 <bek77519101@gmail.com>
2024-09-19 18:38:10 +0900

    feat: publish homepage screen component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,24 @@ import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import TopPropertyCard from "@/libs/components/homepage/TrendPropertyCard";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const  Home: NextPage = () => {
   return (
-      
-    <Stack className={"Home-page"}>
-      
-      <Stack>
-          <Stack className="container">Trend Properties</Stack>
-      </Stack>
-
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-      </Stack>
-
-        <Stack>
-          <Stack className="container">Advertisement</Stack>
-      </Stack>
-
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-      </Stack>
-    
-        <Stack>
-          <Stack className="container">Top Agent</Stack>
-      </Stack>
-       
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
       </Stack>
   );
 }
