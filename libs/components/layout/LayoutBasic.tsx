b7037880b3c838b0f45609f3ab86a59a1aaba150
aris1377 <bek77519101@gmail.com>
2024-09-22 14:18:37 +0900

    feat: feat: Property List Publishing

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,4 +1,3 @@
-
 import { Stack } from "@mui/material";
 import Head from "next/head";
 import Top from "../Top";
@@ -6,40 +5,37 @@ import Footer from "../Footer";
 
 const withLayoutBasic = (Component: any) => {
   return (props: any) => {
-      return (
-        <>
-          <Head>
-            <title>Nestar</title>
-          </Head>
-          <Stack id="pc-wrap">
-            <Stack id={"top"}>
-              <Top />
-            </Stack>
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+        <Stack id="pc-wrap">
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
 
-            <Stack
-              className={"header-basic"}
-              style={{
-                backgroundImage: `url(/img/banner/properties.png)`,
-                backgroundSize: "cover",
-                boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
-              }}
-            >
-              <Stack className={"container"}>
-                <strong>Search</strong>
-                <span>We are glad to see you again!</span>
-              </Stack>
-            </Stack>
-
-            <Stack id={"main"}>
-              <Component {...props} />
-            </Stack>
-
-            <Stack id={"footer"}>
-              <Footer />
+          <Stack className={"header-basic"}
+          style={{
+            backgroundImage: `url(/img/banner/properties.png)`,
+            backgroundSize: "cover",
+            boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+          }}>
+            <Stack className={"container"}>
+              <strong>Search</strong>
+              <span>We are glad to see you again!</span>
             </Stack>
           </Stack>
-        </>
-      );
-    }
+
+          <Stack id={"main"}>
+            <Component {...props} />
+          </Stack>
+
+          <Stack id={"footer"}><Footer/></Stack>
+        </Stack>
+      </>
+    );
   };
+};
+
 export default withLayoutBasic;
